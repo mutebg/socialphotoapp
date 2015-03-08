94a63c4284ecf1d9fa19b4d55d48f71b96f82aa1
stoyan delev <mutebg@gmail.com>
2015-03-08 15:57:44 +0200

    bugs

--- socialapp.sql
+++ socialapp.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `photos` (
 `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `social` enum('facebook','instagram','dropbox') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `path` varchar(37) CHARACTER SET utf8mb4 COLLATE utf8_unicode_ci NOT NULL,
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8_unicode_ci NOT NULL,
+  `social` enum('facebook','instagram','dropbox') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `path` varchar(37) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `date_add` datetime NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
